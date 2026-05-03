d3e74c449cb14ebc29451c5abf0963d291e841eb
ritikravi <ritikravi7724@gmail.com>
2026-05-03 17:34:36 +0530

    Update CORS for rmnastreet.com domain

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }));
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'https://rmnastreet.com',
+    'https://www.rmnastreet.com',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
